f37fbeb74ec3c3ae556b2ba63cfd779c9c63cb7c
ManhBui02 <manh6102002@gmail.com>
2022-08-16 19:20:11 +0700

    baive7

---
+++ index.js
@@ -0,0 +1,12 @@
+let greeting = 'Hello. My name is John';
+let result =greeting.charAt(8);
+alert(result);
+
+let str1 = 'The morning is upon us.';
+let a = (str1.slice(11, 22));
+alert(a);
+
+let str2 = 'Hello';
+let str3 = ' World';
+let str = str2.concat(str3);
+alert(str);
